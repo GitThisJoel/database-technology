89e4f3e902729090bc7d8874a81f01a7b39e9473
GitThisJoel <joelbacker10@gmail.com>
2022-03-10 14:10:09 +0100

    corrected 19-1

--- past_exams/joel/2019/19-1.sql
+++ past_exams/joel/2019/19-1.sql
@@ -15,36 +15,39 @@ CREATE TABLE teacher (
 
 DROP TABLE IF EXISTS class;
 CREATE TABLE class (
+    class_id     TEXT, -- glöm inte detta!!
     semester     TEXT,
     course_code  TEXT,
     lucat        TEXT,
-    PRIMARY KEY(semester),
+    PRIMARY KEY(class_id),
     FOREIGN KEY course_code REFERENCES course(course_code),
     FOREIGN KEY lucat REFERENCES teacher(lucat)
 );
 
 DROP TABLE IF EXISTS exercise;
 CREATE TABLE exercise (
+    exercise_id    TEXT,
     exercise_name  TEXT,
     decription     TEXT,
     duration       REAL,
     course_code    TEXT,
     course_name    TEXT,
     credit         REAL,
-    PRIMARY KEY(exercise_name)
+    PRIMARY KEY(exercise_id)
     FOREIGN KEY course_code REFERENCES course(course_code)
 );
 
 DROP TABLE IF EXISTS session;
 CREATE TABLE session (
+    session_id     TEXT,
     start_time     REAL,  
-    exercise_name  TEXT,
+    exercise_id    TEXT,
     decription     TEXT,
     duration       REAL,
-    semester       TEXT,
-    PRIMARY KEY (start_time)
-    FOREIGN KEY exercise_name REFERENCES exercise(exercise_name)
-    FOREIGN KEY semester REFERENCES class(semester)
+    class_id       TEXT,
+    PRIMARY KEY (session_id)
+    FOREIGN KEY exercise_id REFERENCES exercise(exercise_id)
+    FOREIGN KEY class_id REFERENCES class(class_id)
 );
 
 DROP TABLE IF EXISTS ta;
@@ -61,18 +64,18 @@ CREATE TABLE room (
 
 DROP TABLE IF EXISTS session_ta;
 CREATE TABLE session_ta (
-    start_time  REAL,  
+    session_id  REAL,  
     stil_id     TEXT,
-    PRIMARY KEY(start_time, stil_id),
-    FOREIGN KEY start_time REFERENCES semester(start_time),
+    PRIMARY KEY(session_id, stil_id),
+    FOREIGN KEY session_id REFERENCES semester(session_id),
     FOREIGN KEY stil_id REFERENCES ta(stil_id)
 );
 
-DROP TABLE IF EXISTS seassion_room;
-CREATE TABLE seassion_room (
-    start_time  REAL,  
+DROP TABLE IF EXISTS session_room;
+CREATE TABLE session_room (
+    session_id  TEXT,  
     room_id     TEXT,
-    PRIMARY KEY(start_time, room_id),
-    FOREIGN KEY start_time REFERENCES semester(start_time),
+    PRIMARY KEY(session_id, room_id),
+    FOREIGN KEY session_id REFERENCES semester(session_id),
     FOREIGN KEY room_id REFERENCES room(room_id)
 );
